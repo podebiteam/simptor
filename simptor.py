c88827ce4fbf1a47393548e3a23138b7985a6162
Vicky V. Dasta <vickydasta@ymail.com>
2015-02-27 16:29:31 +0700

    Update simptor.py

--- simptor.py
+++ simptor.py
@@ -42,34 +42,6 @@ def fromb64(s):
 	if s:
 		return b64decode(s)
 
-
-def main():
-	if sys.argv[1] == '-e' or sys.argv[1] == '-E':
-		print '# Encoding Method'
-		while True:
-			
-			inp=raw_input('>>> ')
-			nw=enc(inp)
-			enc_tob64=tob64(nw)
-			print '--> private hash : {}'.format(nw)
-			print '--> base64 encoded : {}'.format(enc_tob64)
-
-	elif sys.argv[1] == '-d' or sys.argv[1]== '-D':
-		print '# Decoding Method'
-		while True:
-			
-			inp=raw_input('>>>')
-			nw=dec(inp)
-			print '--> string : {}'.format(nw)
-
-if __name__ == '__main__':
-	if sys.argv[1] in '-d' or sys.argv[1] in '-e':
-		main()
-	else:
-		print '{} <mode>'.format(sys.argv[0])
-		print '-e < for encryption>'
-		print '-d < for decription>'
-		print 'example : '
-		print 'simptor.py -e '
-		print '>>> do what the fuck you want to do with this script.'
+# it's simplicity will kill you...
+# so,be carefull !!
 	
