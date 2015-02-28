0fe9dc3864bd1d5ee7ee7263f33c4acad3580f22
Vicky V. Dasta <vickydasta@ymail.com>
2015-02-28 20:57:34 +0700

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,14 +1,45 @@
-#!/usr/bin/env/ python
+#! /usr/bin/env python
+# license : under Creative Common License
+# version : 1.1
+# by vickydasta
+# simple plain text hiding tecnique
 
-__author__ = 'vickydasta'
-__doc__    = 'simptorlib usage example'
-
+from base64 import b64encode
+from base64 import b64decode
+from string import maketrans
 import sys
-try:
-	from simptor import enc, tob64, dec,gimerand,mkmd5sum
-except ImportError:
-	print 'cannot import main module.'
-	sys.exit(0)
+import hashlib
+import random
+
+alp='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 ' #everyone can change the chiper table
+trans_alp_to_dd="""!@#$%)(*&^-=_+';:",.<>/?~1[]\|cDklPpBnisUIxvMnCd7809heEJLGOPMN/"""
+trasn_dec=maketrans(trans_alp_to_dd,alp)
+tab_1=maketrans(alp, trans_alp_to_dd)
+
+def mkmd5sum(s):
+	new_s=hashlib.md5(s)
+	return new_s.hexdigest()
+
+def gimerand():
+	'generate random string'
+	rand_="""!@#$%)(*&^-=_+';:",.<>/?~1[]\|cDklPpBnisUIxvMnCd7809heEJLGOPMN"""
+	return random.choice(list(rand_))
+
+def enc(s):return s.translate(tab_1)
+'''encode string with given key
+   for example : 
+   	if given key are '!@#$%' and the plain text is 'father' 
+   	so,father would be same as like '!@#$%' 
+   	and so on
+'''
+
+def dec(s):return s.translate(trasn_dec)
+'''
+   translator function,it's do the reverse job of enc(s)
+'''
+def tob64(s):return b64encode(s)
+def fromb64(s):return b64decode(s)
+def bytencode(s):return enc(s)+giveRandom()
 
 def main():
 	if sys.argv[1] == '-e' or sys.argv[1] == '-E':
@@ -19,7 +50,7 @@ def main():
 			inp=raw_input('>. ')			
 			print '--> private chiper : {}'.format(enc(inp)+gimerand())
 			print '--> base64 encoded : {}'.format(tob64('aaa'))
-			print '--> md5            : {}'.format(mkmd5sum(inp)+gimerand())
+			print '--> md5            : {}'.format(mkmd5sum(inp)+giveRandom())
 
 	elif sys.argv[1] == '-d' or sys.argv[1]== '-D':
 		print '# Decoding Method'
@@ -40,16 +71,3 @@ if __name__ == '__main__':
 		print ' simptor.py -e '
 		print ' >>> do what the fuck you want to do with this script.' 
 	
-
-
-if __name__ == '__main__':
-	if sys.argv[1] == '-d' or sys.argv[1] == '-e':
-		main()
-	else:
-		print '{} <mode>'.format(sys.argv[0])
-		print 'mode: '
-		print '      -e < for encryption>'
-		print '      -d < for decription>'
-		print 'example : '
-		print '       {} -e '.format(sys.argv[0])
-		print '       >>> do what the fuck you want to do with this script.'
