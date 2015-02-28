c3faff25f04322733c5e986f52a4bec4c4b4af11
Vicky V. Dasta <vickydasta@ymail.com>
2015-02-28 16:00:46 +0700

    some silly bugs fixed

--- apps.py
+++ apps.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env/ python
 
 __author__ = 'vickydasta'
-__doc__    = 'simptorlib example'
+__doc__    = 'simptorlib usage example'
 
 import sys
 
@@ -12,30 +12,53 @@ except ImportError:
 	sys.exit(0)
 
 def main():
+	print '\n A Technique for hiding Your Ass'
+
 	if sys.argv > 0:
-		if sys.argv[1] == '-e' or sys.argv[1] == '-E':
-			print '# Encoding Method'
+		if sys.argv[1] == '-e':
+			print '-'*len('text hiding method started'),'>'
+			print ' text hiding method started'
 			while True:
-				inp=raw_input('>>> ')
-				nw=enc(inp)
-				enc_tob64=tob64(nw)
-				print '--> private hash : {}'.format(nw)
-				print '--> base64 encoded : {}'.format(enc_tob64)
+				inp=raw_input('>. ')
+				if inp == '':
+					print 'enter some chipered text...!!!'
+				else:
+					print '--> private hash : {}'.format(enc(inp))
+					print '--> base64 encoded : {}'.format(tob64(nw))
 		elif sys.argv[1] == '-d' or sys.argv[1]== '-D':
 			print '# Decoding Method'
 			while True:
-				inp=raw_input('>>>')
-				nw=dec(inp)
-				print '--> string : {}'.format(nw)
+				inp=raw_input('>. ')
+				if inp == '':
+					pass
+				else:
+					print '--> string : {}'.format(dec(inp))
+		else:
+			print '{} <mode>'.format(sys.argv[0])
+			print 'mode: '
+			print '      -e < for encryption>'
+			print '      -d < for decription>'
+			print 'example : '
+			print '       {} -e '.format(sys.argv[0])
+			print '       >>> do what the fuck you want to do with this script.'
+	else:
+		print '{} <mode>'.format(sys.argv[0])
+		print 'mode: '
+		print '      -e < for encryption>'
+		print '      -d < for decription>'
+		print 'example : '
+		print '       {} -e '.format(sys.argv[0])
+		print '       >>> do what the fuck you want to do with this script.'
 
 
 if __name__ == '__main__':
-	if sys.argv[1] in '-d' or sys.argv[1] in '-e':
+	if sys.argv[1] == '-d' or sys.argv[1] == '-e':
 		main()
 	else:
 		print '{} <mode>'.format(sys.argv[0])
-		print '-e < for encryption>'
-		print '-d < for decription>'
+		print 'mode: '
+		print '      -e < for encryption>'
+		print '      -d < for decription>'
 		print 'example : '
-		print 'simptor.py -e '
-		print '>>> do what the fuck you want to do with this script.'
+		print '       {} -e '.format(sys.argv[0])
+		print '       >>> do what the fuck you want to do with this script.'
