c7ccb6106302fe8dca4a6c8a4f4d206251092b38
Vicky V. Dasta <vickydasta@ymail.com>
2015-02-28 20:52:10 +0700

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,54 +1,46 @@
 #!/usr/bin/env/ python
 
 __author__ = 'vickydasta'
-__doc__    = 'simptorlib usage example'
+__doc__    = 'simptorlib example'
 
 import sys
 
 try:
-	from simptor import enc, tob64, dec
+	from simptor import enc, tob64, dec,gimerand,mkmd5sum
 except ImportError:
 	print 'vick@ubuntu:~# pycompile simptor.py'
 	sys.exit(0)
 
 def main():
-	print '\n A Technique for hiding Your Ass'
+	if sys.argv[1] == '-e' or sys.argv[1] == '-E':
+		print '# Encoding Method'
+		while True:
+			# do we need to unchiper the chiper ? 
+			# i don't think so...
+			inp=raw_input('>. ')			
+			print '--> private chiper : {}'.format(enc(inp)+gimerand())
+			print '--> base64 encoded : {}'.format(tob64('aaa'))
+			print '--> md5            : {}'.format(mkmd5sum(inp)+gimerand())
 
-	if sys.argv > 0:
-		if sys.argv[1] == '-e':
-			print '-'*len('text hiding method started'),'>'
-			print ' text hiding method started'
-			while True:
-				inp=raw_input('>. ')
-				if inp == '':
-					print 'enter some chipered text...!!!'
-				else:
-					print '--> private hash : {}'.format(enc(inp))
-					print '--> base64 encoded : {}'.format(tob64(nw))
-		elif sys.argv[1] == '-d' or sys.argv[1]== '-D':
-			print '# Decoding Method'
-			while True:
-				inp=raw_input('>. ')
-				if inp == '':
-					pass
-				else:
-					print '--> string : {}'.format(dec(inp))
-		else:
-			print '{} <mode>'.format(sys.argv[0])
-			print 'mode: '
-			print '      -e < for encryption>'
-			print '      -d < for decription>'
-			print 'example : '
-			print '       {} -e '.format(sys.argv[0])
-			print '       >>> do what the fuck you want to do with this script.'
+	elif sys.argv[1] == '-d' or sys.argv[1]== '-D':
+		print '# Decoding Method'
+		while True:
+			inp=raw_input('>>>')
+			print '--> md5    : {}'.format(mkmd5sum(inp))
+			print '--> string : {}'.format(dec(inp))
+
+if __name__ == '__main__':
+	if sys.argv[1] in '-d' or sys.argv[1] in '-e':
+		main()
 	else:
-		print '{} <mode>'.format(sys.argv[0])
-		print 'mode: '
-		print '      -e < for encryption>'
-		print '      -d < for decription>'
-		print 'example : '
-		print '       {} -e '.format(sys.argv[0])
-		print '       >>> do what the fuck you want to do with this script.'
+		print ' Usage : {} <mode>'.format(sys.argv[0])
+		print ' Options :'
+		print '          -e < for encryption>'
+		print '          -d < for decription>'
+		print ' example : '
+		print ' simptor.py -e '
+		print ' >>> do what the fuck you want to do with this script.' 
+	
 
 
 if __name__ == '__main__':
